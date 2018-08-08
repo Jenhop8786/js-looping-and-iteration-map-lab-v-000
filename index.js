28b5a152867063537594fcfa3018aac372971cdd
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-08 20:46:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file.
 
-function lowerCaseDrivers (drivers) {
-  return drivers.map (funtion(driver) {
+function lowerCaseDrivers(drivers) {
+  return drivers.map (function(driver) {
     return driver.toLowerCase();
   })
 }
